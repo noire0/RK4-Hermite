b698e55014da7e0c4be9548939515f244a19da40
noire0 <43526958+noire0@users.noreply.github.com>
2020-08-01 14:57:25 -0600

    Add files via upload

---
+++ individual_graph.py
@@ -0,0 +1,34 @@
+#!/bin/python
+import matplotlib.pyplot as plt
+import fnmatch
+import os
+
+def openfile(filename):
+    A = [[],[]]
+    with open(filename) as array:
+        for i in array:
+            A[0].append(float(i.split()[0]))
+            A[1].append(float(i.split()[1]))
+    return A
+
+def addPlot(location):
+    A = dict()
+    for filename in os.listdir(location):
+        if fnmatch.fnmatch(filename, '*.txt'):
+            A[filename] = openfile(location+'/'+filename)
+#            print(filename)
+    return A
+
+def individual(A):
+    for i in A:
+    #    if not fnmatch.fnmatch(i, '*11.5*'):
+        plt.plot(A[i][0],A[i][1],label=i[:-4])
+        plt.legend(loc='upper left')
+        plt.savefig(i[:-4]+'.png')
+        plt.clf()
+
+A = addPlot('fortran/poly')
+B = addPlot('fortran/rk4')
+individual(A)
+individual(B)
+
